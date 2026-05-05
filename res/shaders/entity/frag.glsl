75f7a78c9634fb17b2f4ac53c7d9c032e125367c
R89097 <rosiew2018@hotmail.com>
2026-05-05 15:14:14 +0800

    task B (working) and taske G

--- res/shaders/entity/frag.glsl
+++ res/shaders/entity/frag.glsl
@@ -2,7 +2,8 @@
 #include "../common/lights.glsl"
 
 in VertexOut {
-    LightingResult lighting_result;
+    vec3 ws_frag_position;
+    vec3 ws_normal;
     vec2 texture_coordinate;
 } frag_in;
 
@@ -14,9 +15,50 @@ uniform float inverse_gamma;
 uniform sampler2D diffuse_texture;
 uniform sampler2D specular_map_texture;
 
+//moved from vertex
+uniform vec3 ws_view_position;
+
+uniform vec3 diffuse_tint;
+uniform vec3 specular_tint;
+uniform vec3 ambient_tint;
+uniform float shininess;
+
+#if NUM_PL > 0
+layout (std140) uniform PointLightArray {
+    PointLightData point_lights[NUM_PL];
+};
+#endif
+
 void main() {
-    // Resolve the per vertex lighting with per fragment texture sampling.
-    vec3 resolved_lighting = resolve_textured_light_calculation(frag_in.lighting_result, diffuse_texture, specular_map_texture, frag_in.texture_coordinate);
+    vec3 ws_view_dir = normalize(ws_view_position - frag_in.ws_frag_position);
+
+    LightCalculatioData light_calculation_data = LightCalculatioData(
+        frag_in.ws_frag_position,
+        ws_view_dir,
+        normalize(frag_in.ws_normal)
+    );
+
+    Material material = Material(
+        diffuse_tint,
+        specular_tint,
+        ambient_tint,
+        shininess
+    );
+
+    LightingResult lighting_result = total_light_calculation(
+        light_calculation_data,
+        material
+        #if NUM_PL > 0
+        ,point_lights
+        #endif
+    );
+
+    vec3 resolved_lighting = resolve_textured_light_calculation(
+        lighting_result,
+        diffuse_texture,
+        specular_map_texture,
+        frag_in.texture_coordinate
+    );
 
     out_colour = vec4(resolved_lighting, 1.0f);
     out_colour.rgb = pow(out_colour.rgb, vec3(inverse_gamma));
